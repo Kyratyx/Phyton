1fcb83cfa73f05afc36820e65e939410aadf0764
Juan Pablo Mora <kyratyx@gmail.com>
2020-09-07 23:20:00 -0500

    Programas Terminados

--- TestCaracter.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from EjerciciosPy.Ejerc3 import caracter
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual("an", caracter("amen"))
-if __name__ == '__main__':
-    unittest.main()
--- TestNumeroPar.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from EjerciciosPy.Ejer2 import par
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, par(20))
-if __name__ == '__main__':
-    unittest.main()
